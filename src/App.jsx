0385712d7bac468f4cab3c36be446b400aa5b3e5
Adriana Koch <akoch@oberlin.edu>
2025-05-28 09:55:10 -0400

    refactored layout, navbar, register, and login

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./auth/Register";
 import Login from "./auth/Login";
 import ActivitiesPage from "./activities/ActivitiesPage";
 import Error404 from "./Error404.jsx";
+import { Routes, Route } from "react-router";
 
 /**
  * Fitness Trackr is a platform where fitness enthusiasts can share their workouts and
@@ -11,11 +12,18 @@ import Error404 from "./Error404.jsx";
  * account will be able to upload and manage their own activities.
  */
 export default function App() {
-  const { page } = usePage();
+//  const { page } = usePage();
 
-  if (page === "register") return <Register />;
-  if (page === "login") return <Login />;
-  if (page === "activities") return <ActivitiesPage />;
+//  if (page === "register") return <Register />;
+//  if (page === "login") return <Login />;
+//  if (page === "activities") return <ActivitiesPage />;
+
+  <Routes>
+    <Route path="/" element={<Layout />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/activities" element={<ActivitiesPage />} />
+  </Routes>
 
   return <Error404 />;
 }
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,21 +1,23 @@
 import { useAuth } from "../auth/AuthContext";
-import { usePage } from "./PageContext";
+//import { usePage } from "./PageContext";
+import { useNavigate } from "react-router";
 
 /** Navbar with site navigation links */
 export default function Navbar() {
   const { token, logout } = useAuth();
-  const { setPage } = usePage();
+  const navigate = useNavigate();
+
   return (
     <header>
       <p>Fitness Trackr</p>
       <nav>
-        <a onClick={() => setPage("activities")}>Activities</a>
+        <Link to="/activities">Activities</Link>
         {token ? (
-          <a onClick={() => logout()}>Log out</a>
+          <button onClick={handleLogout}>Log Out</button>
         ) : (
           <>
-            <a onClick={() => setPage("register")}>Register</a>
-            <a onClick={() => setPage("login")}>Login</a>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
           </>
         )}
       </nav>
